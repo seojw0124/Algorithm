e7c6ef28bbdadd5753027505a4d80db23198a9d9
서정우 <89926090+seojw0124@users.noreply.github.com>
2024-06-19 10:08:12 +0900

    [level 0] Title: 암호 해독, Time: 0.08 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.kt
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.kt
@@ -2,10 +2,6 @@ class Solution {
     fun solution(cipher: String, code: Int): String {
         var answer: String = ""
         
-        for (i in 0 .. cipher.length) {
-            if(i != 0 && i % code == 0) answer += cipher[i-1]
-        }
-        
-        return answer
+        return cipher.filterIndexed{ index, c -> (index+ 1) % code == 0 }
     }
 }
