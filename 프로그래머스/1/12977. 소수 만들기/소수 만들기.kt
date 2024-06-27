237773d45ce8d3377793747fef2ef23a1f6d0401
서정우 <89926090+seojw0124@users.noreply.github.com>
2024-06-27 16:26:48 +0900

    [level 1] Title: 소수 만들기, Time: 8.45 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -1,19 +1,20 @@
 class Solution {
     fun solution(nums: IntArray): Int {
         var answer = 0      
-        var sumArr = intArrayOf()
+        var sumList = mutableListOf<Int>()
         
         for (i in 0 until nums.size) {
             for (j in i + 1 until nums.size) {
                 for (k in j + 1 until nums.size) {
                     val sum = nums[i] + nums[j] + nums[k]
-                    sumArr += sum
+                    //if (!sumArr.contains(sum)) sumList.add(sum)
+                    sumList.add(sum)
                 }
             }
         }
-        sumArr.distinct()
+        sumList.distinct()
         var cnt = 0    
-        for (num in sumArr) {
+        for (num in sumList) {
             cnt = (1..num).filter { num % it == 0 }.count()
             if (cnt == 2) answer++
         }
