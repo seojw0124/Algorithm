3a6a7b53b025e8f38466d9cf119221798f64a12b
서정우 <89926090+seojw0124@users.noreply.github.com>
2024-07-02 02:52:44 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 15.16 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        val arr = intArrayOf(6, 5, 4, 3, 2)
+        
+        var answerCnt = 0
+        var zeroCnt = 0
+        
+        for (i in lottos) {
+            if(i == 0) {
+                zeroCnt++
+                continue
+            }
+            for (j in win_nums) {
+                if (i == j) answerCnt++
+            }
+        }
+        
+        val max = if(zeroCnt + answerCnt == 0 || zeroCnt + answerCnt == 1) 6 else arr.indexOf(zeroCnt + answerCnt) + 1
+        val min = if(answerCnt == 0 || answerCnt == 1) 6 else arr.indexOf(answerCnt) + 1 
+        
+        answer += max
+        answer += min
+        
+        return answer
+    }
+}
