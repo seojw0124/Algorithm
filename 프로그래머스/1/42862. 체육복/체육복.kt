6200655fbdaecd25ce6b3a4306c24c3694d2ea6d
서정우 <89926090+seojw0124@users.noreply.github.com>
2024-07-15 09:58:09 +0900

    [level 1] Title: 체육복, Time: 30.81 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var lostList = (lost.sorted() - reserve.sorted()).toMutableList() 
+        var reserveList = (reserve.sorted() - lost.sorted()).toMutableList() 
+        var answer = n - lostList.size
+
+        for( i in lostList){
+            if(reserveList.contains(i - 1)){
+                reserveList.remove(i - 1)
+                answer += 1
+            }
+            else if(reserveList.contains(i + 1)){
+                reserveList.remove(i + 1)
+                answer += 1
+            }
+        }
+        return answer
+    }
+}
